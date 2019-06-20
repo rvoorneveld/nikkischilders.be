bfd746347ee8e4cdb4f4ba9692e3af66b09a06bf
Rick Voorneveld <r.voorneveld@hoteliers.com>
2019-06-20 21:09:12 +0200

    Frontend updates

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -1,7 +1,7 @@
 <div id="{{ $name }}" class="overlay fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center" style="background-color: rgba(0, 0, 0, .75)">
     <a href="#" class="absolute w-full h-full cursor-default"></a>
-    <div class="relative w-5/6 md:w-3/4 lg:w-1/2 max-w-3xl p-12 bg-blue-900 opacity-100">
+    <div class="relative w-full h-full md:h-auto md:w-3/4 lg:w-1/2 max-w-3xl p-6 lg:p-12 bg-blue-900">
         {{ $slot }}
-        <a class="absolute top-0 right-0 mr-3 text-2xl font-extrabold no-underline text-white" href="#">&times;</a>
+        <a class="absolute top-0 right-0 mr-3 text-3xl font-extrabold no-underline text-white" href="#">&times;</a>
     </div>
 </div>
