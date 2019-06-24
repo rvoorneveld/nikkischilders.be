ceb861cb974fcdf62812185703711c779d8212b2
Rick Voorneveld <r.voorneveld@hoteliers.com>
2019-06-24 08:33:20 +0200

    create or update customer based on emailaddress

--- app/Repositories/CustomersRepository.php
+++ app/Repositories/CustomersRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Customer;
+use Illuminate\Http\Request;
 
 class CustomersRepository
 {
@@ -10,6 +11,21 @@ class CustomersRepository
     public function getAllOrderedByLastNameAscending()
     {
         return Customer::orderBy('lastName')->get();
+    }
+
+    public function createOrUpdate(Request $request): Customer
+    {
+        if (null === $customer = Customer::where('emailAddress', $request['emailAddress'])->first()) {
+            $customer = new Customer();
+        }
+
+        $customer->setAttribute($key = 'firstName', $request[$key]);
+        $customer->setAttribute($key = 'lastName', $request[$key]);
+        $customer->setAttribute($key = 'emailAddress', $request[$key]);
+        $customer->setAttribute($key = 'phoneNumber', $request[$key]);
+        $customer->save();
+
+        return $customer;
     }
 
 }
