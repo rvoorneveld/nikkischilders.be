baf704790fd5917843271f0ea3ba0ca59d25ee17
rickvoorneveld <rvoorneveld@me.com>
2019-10-18 15:16:27 +0200

    Added categories table migration

---
+++ database/migrations/2019_10_16_160953_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('categories', static function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title');
+            $table->longText('description');
+            $table->text('image');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+
+}
